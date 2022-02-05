50a9177c44eb1991133d65029b4f0c61449ab16c
Jerry Buaba <49881677+buabaj@users.noreply.github.com>
2022-02-05 16:26:12 +0000

    Update database.py

--- database.py
+++ database.py
@@ -7,7 +7,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 load_dotenv()
 
-SQLALCHEMY_DATABASE_URL = os.getenv("DB_CONN")
+SQLALCHEMY_DATABASE_URL = 'postgresql://ezjfckwlkbtqfp:19ea2be7e380045a0d941e0ab4e7e5391033d0829f4d34afd5097965ca5201fd@ec2-3-219-204-29.compute-1.amazonaws.com:5432/dcqa11k3fhjlus'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
